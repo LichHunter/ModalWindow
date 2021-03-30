456691d4850b3ea566c1fdec85afa32329fe0f50
AlexD2019 <alexander0derevianko@gmail.com>
2021-03-30 13:27:58 +0100

    Added open and close ability for modal window

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
 'use strict';
 
-const modal = document.querySelector(".modal");
-const overlay = document.querySelector(".overlay");
-const btnCloseModal = document.querySelector(".close-modal");
-const btnsOpenModal = document.querySelectorAll(".show-modal");
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+//here we get an array of buttons
+const btnsOpenModal = document.querySelectorAll('.show-modal');
 
 for (let i = 0; i < btnsOpenModal.length; i++) {
-  console.log(btnsOpenModal[i].textContent);
+  btnsOpenModal[i].addEventListener('click', () => openModal());
 }
+
+function openModal() {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+}
+
+function closeModal() {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+}
+
+btnCloseModal.addEventListener('click', () => closeModal());
+overlay.addEventListener('click', () => closeModal())
